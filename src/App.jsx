2d35e42a7348e8e0ea1008d5a4ab1dceefea27ab
akimas1846 <akihiro0923092@gmail.com>
2024-06-18 10:57:43 +0900

    change package and add translation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-function App()
-{
+import Fooder from "./components/Fooder";
+import Header from "./components/Header";
+import Main from "./components/main";
+import React from "react";
 
-}
+function App() {}
 
 export default App;
