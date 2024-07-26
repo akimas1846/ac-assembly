46fe0c9023d875ff1441dc422d46053c94af3789
akimas1846 <akihiro0923092@gmail.com>
2024-07-27 04:33:23 +0900

    footerの編集

--- src/components/Fooder.jsx
+++ src/components/Fooder.jsx
@@ -2,7 +2,9 @@ import React from "react";
 function Fooder() {
   return (
     <footer className="footer">
-      <p>頑張ってる</p>
+      <p>
+        本ページは、FROMSOFTWARE動画、画像の投稿に関するガイドラインに準じています。
+      </p>
     </footer>
   );
 }
